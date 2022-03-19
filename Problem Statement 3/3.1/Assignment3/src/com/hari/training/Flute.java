8a75975f49fd36c74acbffab22d3fe285c32f714
rajeshwarjagtap <97775246+rajeshwarjagtap@users.noreply.github.com>
2022-03-19 17:59:25 +0530

    Add files via upload

---
+++ Problem Statement 3/3.1/Assignment3/src/com/hari/training/Flute.java
@@ -0,0 +1,11 @@
+package com.hari.training;
+
+public class Flute extends Instrument {
+
+	@Override
+	public void play() {
+		System.out.println("Flute is playing  toot toot toot toot");
+
+	}
+
+}
---
+++ Problem Statement 3/3.1/Assignment3/src/com/hari/training/Instrument.java
@@ -0,0 +1,5 @@
+package com.hari.training;
+
+public abstract class Instrument {
+	public abstract void play();
+}
